51511a72eef347943519b72d9af00aa2381ab1f1
admin <admin@DESKTOP-9K6N0AO>
2024-03-29 18:00:08 +0900

    240329 개발환경 구축

---
+++ src/main/java/com/example/demo/BestdonutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestdonutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestdonutApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/test/mapper/TestMapper.java
@@ -0,0 +1,8 @@
+package com.example.demo.test.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestMapper {
+	String getText();
+}
