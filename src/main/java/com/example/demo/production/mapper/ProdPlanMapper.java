5e5a3885ef42c2f82040424a74ff222840fae835
qpqp12121 <qpqp12121@naver.com>
2024-04-01 19:24:40 +0900

    20240401 박정언 7:30

---
+++ src/main/java/com/example/demo/production/mapper/ProdPlanMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo.production.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.production.ProdPlanVO;
+
+@Mapper
+public interface ProdPlanMapper {
+
+	//생산요청
+	public List<ProdPlanVO> getProdReq();
+	public List<ProdPlanVO> getProdReqDetail();
+	
+}
