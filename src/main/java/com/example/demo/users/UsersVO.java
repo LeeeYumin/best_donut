5f2eb829415c54829e555f93ca0eab1a1281da77
shinhw91 <shinhw48@gmail.com>
2024-04-08 17:29:28 +0900

    20240408 신혜원 사용자 등록 기능 완료

--- src/main/java/com/example/demo/users/UsersVO.java
+++ src/main/java/com/example/demo/users/UsersVO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 public class UsersVO {
--- src/main/resources/static/js/users/insertUsers.js
+++ src/main/resources/static/js/users/insertUsers.js
@@ -53,29 +53,56 @@ async function saveUsers() {
 	.then(res => res.json())
 	.then(res => {
     console.log(res);
+    result = res;
   })
   
-  alert('등록완료!');
+	if(result){
+		Swal.fire({
+			position: "center",
+			icon: "success",
+			title: "사용자 등록완료!",
+			text: "주문등록이 정상적으로 처리되었습니다.",
+			showConfirmButton: false,
+			timer: 2000
+		});
+	}
+	else {
+		Swal.fire({
+			position: "center",
+			icon: "error",
+			title: "주문등록 실패",
+			text: "주문등록이 정상적으로 처리되지 않았습니다.",
+			showConfirmButton: false,
+			timer: 2000
+		});
+	}
 };
 
+
 function formValidation() {
-  if(insertForm.usersCode.value == null || insertForm.usersCode.value == '') {
-    return false;
-  }
+  const notice1 = document.getElementById('notice1');
+  const notice2 = document.getElementById('notice2');
+  const notice3 = document.getElementById('notice3');
+  const notice4 = document.getElementById('notice4');
 
   if(insertForm.usersName.value == null || insertForm.usersName.value == '') {
+    notice1.innerText = '사용자명을 입력하세요.';
     return false;
   }
 
   if(insertForm.position.value == null || insertForm.position.value == '') {
+    notice2.innerText = '소속을 입력하세요.';
     return false;
   }
 
-  if(insertForm.perm.value == null || insertForm.perm.value == '') {
+  const permList = document.querySelectorAll("[name=perm]:checked");
+  if(permList == null || permList.length == 0) {
+    notice3.innerText = '사용자권한을 선택하세요.';
     return false;
   }
 
   if(insertForm.status.value == null || insertForm.status.value == '') {
+    notice4.innerText = '사용자상태를 선택하세요.';
     return false;
   }
 
