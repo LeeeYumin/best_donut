12502055916c48629bcea665249d3bb9a7574cb4
qpqp12121 <qpqp12121@naver.com>
2024-04-08 17:30:57 +0900

    20240408 박정언 생산계획 삭제 후처리하기

--- src/main/resources/static/js/production/prodPlanList.js
+++ src/main/resources/static/js/production/prodPlanList.js
@@ -72,11 +72,11 @@ getProdPlanList()
 		// 생산계획 목록 조회(ajax) -검색포함
 		async function getProdPlanList(){
 
-			const startDate = document.getElementById('searchStartDate').value;
-			const endDate = document.getElementById('searchEndDate').value;
-			const planCode = document.getElementById('prodPlanCode').value;
+			const searchStartDate = document.getElementById('searchStartDate').value;
+			const searchEndDate = document.getElementById('searchEndDate').value;
+			const prodPlanCode = document.getElementById('prodPlanCode').value;
 
-			const obj = {startDate, endDate, planCode};
+			const obj = {searchStartDate, searchEndDate, prodPlanCode};
 			console.log(obj);
 			
 			const data = {
@@ -94,7 +94,7 @@ getProdPlanList()
 		};
 
 		//검색버튼
-		document.getElementById('searchBtn').addEventListener('click', getProdPlanList());
+		document.getElementById('searchBtn').addEventListener('click', getProdPlanList);
 		
 		//초기화버튼
 		document.getElementById('resetBtn').addEventListener('click', function() {
@@ -128,50 +128,166 @@ getProdPlanList()
 					align: 'center'
 				},
 				{
+					header : '제품명',
+					name : 'productName',
+					align: 'center'
+				},
+				{
 					header : '고정수량',
 					name : 'fixCnt',
-					align: 'center'
+					align: 'center',
+					editor: 'text',
+					formatter: function(price) {
+						return priceFormat(price.value);
+					},
 				}, 
 				{
 					header : '요청수량',
 					name : 'reqCnt',
-					align: 'center'
+					align: 'center',
+					formatter: function(price) {
+						return priceFormat(price.value);
+					},
 				},
 				{
 					header : '계획수량',
 					name : 'planCnt',
-					align: 'center'
+					align: 'center',
+					formatter: function(price) {
+						return priceFormat(price.value);
+					}
 				},
 				{
 					header : '미지시수량',
 					name : 'notInstructCnt',
-					align: 'center'
+					align: 'center',
+					formatter: function(price) {
+						return priceFormat(price.value);
+					},
 				},
 				{
 					header : '지시수량',
 					name : 'instructDoneCnt',
-					align: 'center'
+					align: 'center',
+					formatter: function(price) {
+						return priceFormat(price.value);
+					},
 				},
 				
 			]
 		});
 
 		//생산계획 클릭 시 아래 생산계획상세내용 출력
-		plList.on("click", (e) => {
-			let plCode = plList.getValue(e.rowKey, "prodPlanCode")
-			getProdPlanAll(plCode);        
-			console.log("prodPlanCode: " + plCode);      
-		});
+		plList.on('click', e => {
+			let status = plList.getValue(e.rowKey, "prodPlanStatus");
+			let plCode = plList.getValue(e.rowKey, "prodPlanCode");
+
+			//계획상세목록
+			getProdPlanAll(plCode);
+
+			//계획상태가 미지시이면 수정가능하게
+			if(status == 'LS1') {
+				plAll.enableColumn('fixCnt');
+
+			}else { //아니면 수정불가
+				plAll.disableColumn('fixCnt');
+			}		
+		})
 		
  		async function getProdPlanAll(plCode){
 			await fetch(`/ajax/prodPlanAll?prodPlanCode=${plCode}`)
 			.then(res => res.json())
 			.then(res => {
-				console.log(res);
+				//console.log(res);
 	
 				plAll.resetData(res);
 			})
 		};
+
+		//수정 시 변경 값 계산
+		plAll.on('afterChange', e => {
+			let row = plAll.getRow(e.changes[0].rowKey);
+			let fixCnt = parseInt(row.fixCnt);
+			let reqCnt = parseInt(row.reqCnt);
+
+			let planCnt = parseInt(fixCnt + reqCnt);
+			//console.log(planCnt)
+			let notInstructCnt = planCnt;
+
+			plAll.setValue(row.rowKey, 'planCnt', planCnt);
+			plAll.setValue(row.rowKey, 'notInstructCnt', notInstructCnt);
+		})
+
+		//생산계획 상세 수정하기
+		async function updatePlanDetail() {
+			plAll.blur();
+			const plDe = plAll.getModifiedRows().updatedRows
+			console.log(plDe);
+			
+
+			await fetch('ajax/updateProdPlanDetail', {
+				method: 'post',
+				headers: jsonHeaders,
+				body : JSON.stringify(plDe)
+			})
+			.then(res => res.json())
+			.then(res => {
+				console.log(res);
+				if(res == 1) {
+					alert('수정되었습니다.');
+				}else if(res) {
+					alert('수정 중 오류 발생');
+				}
+			})
+		}
+
+		//생산계획 상태 미지시일 경우에만 삭제하기
+		plList.on('click', e => {
+			const status = plList.getValue(e.rowKey,"prodPlanStatus");
+			console.log(status);
+
+			if(status == 'LS1') {
+
+			}
+		});
+
+
+		async function deletePlan() {
+			
+			let row = plList.getFocusedCell().rowKey;
+			let plan = plList.getData()[row];
+
+			//계획상세
+			plan.dvo =  plAll.getData();
+
+			if(plan.prodPlanStatus == 'LS1') {
+				await fetch('ajax/deleteProdPlan', {
+					method: 'post',
+					headers: jsonHeaders,
+					body : JSON.stringify(plan)
+				})
+				.then(res => res.json())
+				.then(res => {
+					console.log(res);
+					if(res.result == 1) { //controller에서 "result"로 값 넘김
+						alert('삭제되었습니다.');
+					}else {
+						alert('삭제 중 오류 발생');
+					}
+				})
+			} else {
+				alert('지시등록된 건으로 삭제 불가');
+			}
+		}
+		//삭제응답 (그리드에 입력된 모든 정보 비우기)
+		// function saveRes(res) {
+		// 	plDeInsert.resetData([]);
+		// 	plreq.resetData([]);
+		// 	plreqD.resetData([]);
+		// }
+
+
+		
 
 		
 
