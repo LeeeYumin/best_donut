326c19525906af8f291d24edd4338207a5b95e0d
LeeeYumin <152114081+LeeeYumin@users.noreply.github.com>
2024-04-11 17:26:30 +0900

    20240411 BOM페이지구현중

--- src/main/java/com/example/demo/bom/BomVO.java
+++ src/main/java/com/example/demo/bom/BomVO.java
@@ -17,15 +17,20 @@ public class BomVO {
 	private String applyStatus;
 	private String productCode;
 	private String usersCode;
+	private String procName; //PROC 컬럼에서 추가함. 공정이름 
 	
 	// bom_detail 칼럼들 
 	private String bomDetailCode;
-	private Long needCnt;
-	private String unit;
-	private String matCode;
+	private Long needCnt; //소요수량
+	private String unit; //단위
+	private String matCode; //자재코드
 	
 	// 자재 발주를 위한 칼럼들
-	private Integer unitPrice;
-	private String matName;
+	private Integer unitPrice; //단가
+	private String matName; //자재명
+	
+	//product 완제품명 칼럼
+	private String productName; 
+	
 	
 }
--- src/main/java/com/example/demo/bom/service/BomService.java
+++ src/main/java/com/example/demo/bom/service/BomService.java
@@ -6,4 +6,9 @@ import com.example.demo.bom.BomVO;
 
 public interface BomService {
 	public List<BomVO> getMatOrdersBom();
+	
+	public List<BomVO> getProdBom();
+	public List<BomVO> getListBom();
+	public List<BomVO> bomselList();
+	
 }
