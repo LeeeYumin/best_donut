5e3876644237171d1882d39c54e7de7403f02bba
shinhw91 <shinhw48@gmail.com>
2024-04-20 17:25:28 +0900

    20240420 신혜원 파일업로드 기능 보완

--- src/main/java/com/example/demo/config/ResourceConfiguration.java
+++ src/main/java/com/example/demo/config/ResourceConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,12 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResourceConfiguration implements WebMvcConfigurer {
 
+	@Value("${uploadFolder}") 
+    private String uploadFolder;
+	
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-	System.out.println("=============================================>>>>>>>>>>>>>>>>>>>>>");
+    	String path = "file:///"+uploadFolder+"/";
+	System.out.println("=============================================>>>>>>>>>>>>>>>>>>>>>"+ path);
         registry.addResourceHandler("/download/**")
-        .addResourceLocations("file:///C:/donutFile/")      
-        
+      //  .addResourceLocations("file:///"+uploadFolder+"/") 
+        .addResourceLocations(path)
         // 접근 파일 캐싱 시간 
 	.setCacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES));
     }
